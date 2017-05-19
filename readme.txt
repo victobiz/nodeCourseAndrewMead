8242ea394f2c18fd925b97eaa25b25727243a72a
chris victores <victobiz@yahoo.com>
2017-05-19 15:51:23 -0400

    initial commit

---
+++ readme.txt
@@ -0,0 +1,3 @@
+use express get and send methods and set up a server on port 3000. require express. create a public folder
+and use middleware (create our own) to set up a page using the app.use and .static methods. Next
+use template packages like ejs, handlebar to create a view.
---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+//key is view engine create a folder called views the default directory in express for views
+app.set('view engine', 'hbs');
+//variable that stores path to express directory (node-web server)
+
+//use calls some middlewear
+app.use(express.static(__dirname +'/public'));
+app.use((req,res,next) =>{
+var now = new Date().toString();
+var log =(`${now}: ${req.method} ${req.url}`);
+
+  console.log(log);
+//  fs.appendFile('server.log', log + '\n');
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log')
+    }
+  });
+
+  next();
+});
+
+app.use((req,res, next) =>{
+  res.render('maintanance.hbs');
+});
+hbs.registerHelper('getCurrentYear', () =>{
+  return 'test';//new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+// app.get('/', (req, res) =>{
+//   res.send('<h1>goodbye</h1>');
+//
+// });
+
+app.get('/', (req, res) =>{
+
+  res.send({
+    name: 'chris',
+    likes: ['money', 'hitler']
+  });
+});
+
+app.get('/about', (req, res) => {
+  //res.send('about Page')
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+
+});
+
+app.get('/home', (req, res) =>{
+  res.render('home.hbs', {
+    welcomeMessage: 'hello buddy!',
+    pageTitle: 'Home page',
+    currentYear: new Date().getFullYear()
+  })
+});
+
+//create a route at /bad  respond using response.send send back josn with error message
+//property then go to /bad make sure it shows up.
+app.get('/bad' , (req, res) => {
+  res.send({
+    error: 'you fucked up'
+  })
+})
+
+app.listen(3000);
